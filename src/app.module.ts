08ea7e0e3578c5cc32b7483c05ceb6a70fa11d10
IWhitebird <115157819+IWhitebird@users.noreply.github.com>
2023-09-10 18:30:51 +0530

    updated bot

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegrambotModule } from './telegrambot/telegrambot.module';
 import  { ConfigModule} from '@nestjs/config'
+import {MongooseModule} from '@nestjs/mongoose'
 
 @Module({
-  imports: [ConfigModule.forRoot() ,TelegrambotModule],
+  imports: [ConfigModule.forRoot() ,TelegrambotModule , MongooseModule.forRoot(process.env.MONGO_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/telegrambot/telegrambot.service.ts
+++ src/telegrambot/telegrambot.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@nestjs/common';
 const TelegramBot = require('node-telegram-bot-api');
-
+const request = require('request')
 
 @Injectable()
 export class TelegrambotService {
     private bot : any;
-    
     constructor() {
 
         this.bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
-        
+
+
+        this.bot.onText(/\/help/, (msg : any) => {
+            const chatId = msg.chat.id;
+            this.bot.sendMessage(chatId, 
+`I'm your Weather bot. I can provide you with daily weather updates. 
+/start - to start the subscription process
+/unsubscribe - to unsubscribe from weather updates
+/update - to update your location
+/help - to see this message again
+`);
+          });
+
+
         this.bot.onText(/\/start/, (msg : any) => {
             const keyboard = {
                 inline_keyboard: [
-                    [{ text: 'Subscribe', callback_data: 'yes' }],
+                    [{ text: 'Subscribe', callback_data: 'yes'}],
                     [{ text: 'No', callback_data: 'no' }]
                 ]
             };
@@ -26,56 +38,100 @@ export class TelegrambotService {
 
         });
 
-        this.bot.on('callback_query', (query) => {
-            console.log(query)
+        this.bot.on('callback_query', (query : any) => {
             const chatId = query.message.chat.id;
             const data = query.data;
  
             if (data === 'yes') {
-                this.subscribeToWeatherUpdates(chatId);  
+                
+                this.subscribeToWeatherUpdates(query);
+                
             } else if (data === 'no') {
-                this.bot.sendMessage(chatId, 'Oh, thats sad. You can always subscribe to our daily weather updates by typing /start');
+                this.bot.sendMessage(chatId, 'Oh, thats sad. You can always subscribe to our daily weather updates by typing /subscribe and /start for more commands');
             }
         });
 
-        
 
     }
-
-
-    askForLocation(chatId: string) {
+    
+    subscribeToWeatherUpdates(query : any) {
+        
+        const chatId = query.message.chat.id;
+        this.askForLocation(chatId)
+    }
+    
+    askForLocation(chatId : string) : any {
         const locationRequest = {
-            text: 'Please share your location with us so we can provide accurate weather updates.',
-            chat_id: chatId,
+            text: 'Please share your location with me',
             reply_markup: {
-                keyboard: [[{ text: 'Share Location', request_location: true }]],
+                resize_keyboard: true,
                 one_time_keyboard: true,
-            },
+                keyboard: [
+                    [
+                        {
+                            text: 'Share Location',
+                            request_location: true
+                        }
+                    ],
+                ],
+            }
         };
-
+    
         this.bot.sendMessage(chatId, locationRequest.text, {
             reply_markup: JSON.stringify(locationRequest.reply_markup)
+        }).then(() => {
+            this.bot.once('location', (msg : any) => {
+                const userChatId = msg.chat.id;
+
+                const hideKeyboard = {
+                    remove_keyboard: true,
+                };
+                this.bot.sendMessage(userChatId,`Thanks for sharing your location...`, {
+                    reply_markup: JSON.stringify(hideKeyboard)
+                });
+
+                console.log(msg)
+
+                const lat = msg.location.latitude;
+                const lon = msg.location.longitude;
+
+                const query = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEAPTHER_AP_KEY}`;
+                console.log(query)
+
+                request(query, (error, response, body) => {
+                    if (!error && response.statusCode == 200) {
+                        const res = JSON.parse(body);
+                        const temp = Math.round(res.main.temp - 273.15);
+                        const pressure = Math.round(res.main.pressure - 1013.15);
+                        const rise = new Date(res.sys.sunrise * 1000);
+                        const set = new Date(res.sys.sunset * 1000);
+
+                        const weatherInfo = `
+@@@@ ${res.name} @@@@ \n
+Temperature: ${temp}°C
+Humidity: ${res.main.humidity}%
+Weather: ${res.weather[0].description}
+Pressure: ${pressure} atm
+Sunrise: ${rise.toLocaleTimeString()}
+Sunset: ${set.toLocaleTimeString()}
+Country: ${res.sys.country}
+`;
+
+                        this.bot.sendMessage(chatId, weatherInfo, { parse_mode: "Markdown" });
+                        
+                    } else {
+                        console.error('Error fetching weather data:', error);
+                    }
+                });
+            });
+
+        }).catch((error , reject) => {
+            console.error('Error sending location request:', error);
+            reject(error);
         });
-
-        this.bot.once('location', (msg : any) => {
-            console.log(msg);
-            const chatId = msg.chat.id;
-            const lat = msg.location.latitude;
-            const lon = msg.location.longitude;
-    
-            this.bot.sendMessage(chatId, `Thanks, we will provide weather updates for ${lat} ${lon}`);
-        });
-        
     }
 
-
-    subscribeToWeatherUpdates(chatId: string) {
-        this.askForLocation(chatId);
-
-        this.bot.sendMessage(chatId, 'Great! Thanks for subscribing to our daily weather updates.');
-    }
-    
-    sendMessage(chatId: string, message: string) {
-        this.bot.sendMessage(chatId, message);
-    }
 }
+
+
+//`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${process.env.WEATHER_API_KEY}`
